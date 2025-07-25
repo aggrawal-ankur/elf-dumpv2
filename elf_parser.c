7f9f42b9ed41b49e8edee8fb9fc61996b8da4fff
hi-anki <managemee@protonmail.com>
2025-07-25 08:16:52 +0530

    parsed magic number part

---
+++ elf_parser.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <elf.h>
+#include "elf_parser.h"
+
+int parse_elf(const char *filename){
+  // Open the file
+  FILE *file_object = fopen(filename, "rb");
+
+  // Check if the file is opened correctly
+  if (!file_object){
+    perror("Error: file can't be opened! `fopen()`\n");
+    return -1;
+  }
+
+  // Data type for elf_header struct, coming from the elf.h file 
+  Elf64_Ehdr elf_header;
+
+  // Reading the first 64-bytes directly into our elf_header struct
+  // fread()'s signature is as follows: 
+  if (fread(&elf_header, 1, sizeof(Elf64_Ehdr), file_object) != sizeof(Elf64_Ehdr)) {
+    perror("Error: Not a valid ELF! `fread()`\n");
+    fclose(file_object);
+    return -1;
+  }
+
+  // We know that an ELF binary start with 4 magic bytes. These are `0x7f`, `E`, `L`, `F`
+  // Our next task is to check this
+  if (elf_header.e_ident[0] != 0x7f ||
+      elf_header.e_ident[1] != 'E' ||
+      elf_header.e_ident[2] != 'L' ||
+      elf_header.e_ident[3] != 'F')
+  {
+    fprintf(stderr, "Error: Magic number mismatch.\n");
+    fclose(file_object);
+    return -1;
+  }
+  printf("Valid ELF file detected.\n  Entry point: 0x%lx\n", elf_header.e_entry);
+
+  fclose(file_object);
+  return 0;
+}
