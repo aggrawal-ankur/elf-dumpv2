cb27efa06dc7e5f42f85973a85f7874c51246887
hi-anki <managemee@protonmail.com>
2025-07-29 12:09:50 +0530

    done documenting it

--- core_api/verify_elf.c
+++ core_api/verify_elf.c
@@ -1,22 +1,20 @@
+// Explanation at [GitBook](https://ankuragrawal.gitbook.io/home/my-first-c-project-an-elf-parser-and-interpreter/magic-verification-api)
 #include <stdio.h>
+#include <string.h>
 #include "verify_elf.h"
 
 int verify_elf(FILE* file_object){
-
-  // Store the 4 bytes of magic number
   unsigned char magic_number[4];
 
-  // Reading the first 4-bytes into our magic_number array
+  fseek(file_object, 0, SEEK_SET);
   if (fread(&magic_number, 1, 4, file_object) != 4) {
     fprintf(stderr, "Error: `fread()`: Unable to read ELF magic bytes.\n");
     fclose(file_object);
     return -1;
   }
-  
-  // We know that an ELF binary start with 4 magic bytes. These are `0x7f`, `E`, `L`, `F`.
-  // Lets verify this.
+
   if (magic_number[0] != 0x7f || magic_number[1] != 'E' || magic_number[2] != 'L' || magic_number[3] != 'F'){
-    fprintf(stderr, "Error: Unexpected magic bytes returned.\n  Expected: `0x7F, E, L, F`\n  Found: %c, %c, %c, %c\n", magic_number[0], magic_number[1], magic_number[2], magic_number[3]);
+    fprintf(stderr, "Error: Unexpected magic bytes returned.\n  Expected: `0x7F, E, L, F`\n  Found: %02X, %02X, %02X, %02X\n", magic_number[0], magic_number[1], magic_number[2], magic_number[3]);
     fclose(file_object);
     return -1;
   }
