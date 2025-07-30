1ed4c5f4855130672ddd79b0a0fdcc5f9daf6b16
hi-anki <managemee@protonmail.com>
2025-07-30 05:40:59 +0530

    file object variable made consistent

--- main.c
+++ main.c
@@ -9,15 +9,15 @@ int main(int argc, char *argv[]){
   }
 
   printf("Opening the file....\n");
-  FILE* file_object = fopen(argv[1], "rb");
-  if (!file_object){
+  FILE* f_obj = fopen(argv[1], "rb");
+  if (!f_obj){
     fprintf(stderr, "  Error: `fopen()`: file can't be opened!\n");
     return -1;
   }
   printf("  File opened successfully!\n");
 
-  printf("Checking for ELF....\n");
-  if (verify_elf(file_object) != 0){
+  printf("Checking for ELF validity....\n");
+  if (verify_elf(f_obj) != 0){
     fprintf(stderr, "  Error: `parse_elf()`: Failed to verify the ELF file.\n");
     return 1;
   }
@@ -25,7 +25,7 @@ int main(int argc, char *argv[]){
 
   Elf64_Ehdr ehdr;
   printf("Parsing file headers....\n");
-  if (parse_ehdr(file_object, &ehdr) != 0){
+  if (parse_ehdr(f_obj, &ehdr) != 0){
     fprintf(stderr, "  Error: file headers can't be parsed!\n");
     return -1;
   }
@@ -34,7 +34,7 @@ int main(int argc, char *argv[]){
   Elf64_Phdr* phdrs = NULL;
   int phdr_count = 0;
   printf("Parsing program headers....\n");
-  if (parse_phdrs(file_object, &ehdr, &phdrs, &phdr_count) != 0){
+  if (parse_phdrs(f_obj, &ehdr, &phdrs, &phdr_count) != 0){
     fprintf(stderr, "  Error: program headers can't be parsed!\n");
     return -1;
   }
@@ -43,7 +43,7 @@ int main(int argc, char *argv[]){
   Elf64_Shdr* shdrs = NULL;
   int shdr_count = 0;
   printf("Parsing section headers....\n");
-  if (parse_shdrs(file_object, &ehdr, &shdrs, &shdr_count) != 0){
+  if (parse_shdrs(f_obj, &ehdr, &shdrs, &shdr_count) != 0){
     fprintf(stderr, "  Error: section headers can't be parsed!\n");
     return -1;
   }
@@ -53,7 +53,7 @@ int main(int argc, char *argv[]){
   char** out_shstrtab = NULL;
   int sh_entry_count = 0;
   printf("Parsing section header string table....\n");
-  if (parse_shstrtab(file_object, &ehdr, &out_raw_shstrtab, &out_shstrtab, &sh_entry_count) != 0){
+  if (parse_shstrtab(f_obj, &ehdr, &out_raw_shstrtab, &out_shstrtab, &sh_entry_count) != 0){
     fprintf(stderr, "  Error: section header string table can not be parsed.\n");
     return -1;
   }
@@ -62,12 +62,12 @@ int main(int argc, char *argv[]){
   char** out_strtab = NULL;
   int str_entry_count = 0;
   printf("Parsing string table....\n");
-  if (parse_strtab(file_object, &ehdr, &out_strtab, &str_entry_count) != 0){
+  if (parse_strtab(f_obj, &ehdr, &out_strtab, &str_entry_count) != 0){
     fprintf(stderr, "Error: string table can not be parsed.\n");
     return -1;
   }
   printf("  String table parsed successfully.\n");
 
-  fclose(file_object);
+  fclose(f_obj);
   return 0;
 }
